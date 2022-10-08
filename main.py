432960b6361fab11399e31371d9e6f73323c68d1
unknown <dima2balca2@gmail.com>
2022-10-08 23:24:22 +0300

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Какое у тебя имя мне интересно:")
-name = input()
-print("Привет, " +  name + "!")
+class Cinema:
+    def __init__(self):
+        self.name = input('ведите имя')
+        self.rating = input('ведите rating')
+    def printInfo(self):
+        print(f'{self.name} {self.rating}')
+
+cinemas = []
+cinemas.append(Cinema())
+cinemas.append(Cinema())
+cinemas.append(Cinema())
+
+for i in range(len(cinemas)):
+    cinemas[i].printInfo()
